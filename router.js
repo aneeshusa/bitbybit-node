227f540eb19cacb5fd0b49e95fce3ca93eb0244f
Aneesh Agrawal <aneeshusa@gmail.com>
2014-03-29 23:57:11 -0400

    Add POST Processing Module

--- router.js
+++
@@ -1,3 +0,0 @@
-var qs = require("querystring");
-
-
--- verify.js
+++ verify.js
@@ -1,11 +1,24 @@
 var http = require("http");
+var processPost = require("./processPost.js");
 
 function start() {
     function onRequest(request, response) {
         console.log("Request Received");
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World!");
-        response.end();
+        if (request.method == "POST") {
+            processPost.processPost(request, response, function() {
+                console.log(response.post);
+                console.log("POST Request Received");
+                response.writeHead(200, {"Content-Type": "text/plain"});
+                response.write("Thanks for the POST request!");
+                response.end();
+            }
+            );
+        } else {
+            console.log("Non Post Request Received");
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write("Next time, trust your POST to snail mail!");
+            response.end();
+        }   
     }
 
     http.createServer(onRequest).listen(8888, "127.0.0.1");
